a512562b1d265662343eebbf0f68e730d2a19e3e
rafael.cunha <rafael.cunha@rfalabs.ca>
2023-10-13 18:12:24 -0300

    Initial Commit

---
+++ ChainList/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ChainList
+//
+//  Created by Rafael Cunha on 2023-10-13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
